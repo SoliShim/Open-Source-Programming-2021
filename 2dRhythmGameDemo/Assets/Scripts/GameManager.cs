1849856c6e786750ccede0801dd7a71d49af26fa
Sungmin Shim <shimsungmin1221@gmail.com>
2021-11-16 15:14:25 +0900

    uploaded 2dRhythmGameDemo unity file  by Sungmin Shim

---
+++ 2dRhythmGameDemo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public BeatScroller theBS;
+
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
+    public float totalNotes;
+    public float normalHits;
+    public float goodHits;
+    public float perfectHits;
+    public float missedHits;
+
+    public GameObject resultsScreen;
+    public Text percentHitText, normalsText, goodsText, perfectsText, missesText, rankText, finalScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
+
+        totalNotes = FindObjectsOfType<NoteObject>().Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        } else
+        {
+            if(!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            {
+                resultsScreen.SetActive(true);
+
+                normalsText.text = "" + normalHits;
+                goodsText.text = goodHits.ToString();
+                perfectsText.text = perfectHits.ToString();
+                missesText.text = "" + missedHits;
+
+                float totalHit = normalHits + goodHits + perfectHits;
+                float percentHit = (totalHit / totalNotes) * 100f;
+
+                percentHitText.text = percentHit.ToString("F1") + "%";
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit on Time");
+
+        if(currentMultiplier -1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if(multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+        normalHits++;
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+        goodHits++;
+    }   
+    
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
+        perfectHits++;
+    }
+
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+        
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        missedHits++;
+    }
+}
